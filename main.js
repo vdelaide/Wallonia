d1c3a0393062722b09505fc7ac77200deaf06026
vdelaide <adelaidemt9479@gmail.com>
2023-06-30 17:11:57 -0500

    Updated the HTML to be more organized, CSS to assign places, JS to start on the physics of the hero banner
    Start

--- main.js
+++ main.js
@@ -1,39 +1,60 @@
 var Hero = Hero || {};
+const canvasElem = document.getElementById("canvas");
 
 Hero.banner = function(){
+
+    /** Set up relative positions and scales **/
+    var VIEW = {};
+
+    VIEW.width    = window.innerWidth;
+    VIEW.height   = window.innerHeight;
+
+    VIEW.centerX  = VIEW.width / 2;
+    VIEW.centerY  = VIEW.height / 2;
+
+    VIEW.offsetX  = VIEW.width / 2;
+    VIEW.offsetY  = VIEW.height / 2;
+
     // module aliases
     var Engine = Matter.Engine,
         Render = Matter.Render,
         Runner = Matter.Runner,
         Bodies = Matter.Bodies,
+        Mouse  = Matter.Mouse,
         Composite = Matter.Composite,
-        MouseConstraint = Matter.MouseConstraint,
-        Mouse = Matter.Mouse;
+        MouseConstraint = Matter.MouseConstraint;
 
     // create an engine
-    var engine = Engine.create();
+    var engine = Engine.create(), world = engine.world;
 
     // create a renderer
     var render = Render.create({
-        element: document.body,
-        engine: engine
+
+        canvas:  canvasElem, //assigns the canvas element
+        element: document.getElementById("debug"), //assigns parent element of the canvas
+        engine:  engine
+
     });
 
-    // create two boxes and a ground
-    var boxA = Bodies.rectangle(400, 200, 80, 80);
-    var boxB = Bodies.rectangle(450, 50, 80, 80);
-    var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+    //bodies.rectangle(x, y, width, height)
+    Composite.add(world, [
 
-    // add all of the bodies to the world
-    Composite.add(engine.world, [boxA, boxB, ground]);
+        // ground
+        ground    = Bodies.rectangle(0, -75, VIEW.width, 10, { isStatic: true }),
 
-    // run the renderer
-    Render.run(render);
+        // walls
+        ceiling   = Bodies.rectangle(0, 0, VIEW.width, 10, {isStatic: true}),
+        wallRight = Bodies.rectangle(0, 0, 80, 80, {isStatic: true}),
+        wallLeft  = Bodies.rectangle(0, 200, 80, 80, {isStatic: true}),
 
-    // create runner
+        //testing purposes
+        test      = Bodies.rectangle(0, 0, 20, 20)
+
+      ]);
+
+    // run the renderer, runner, and engine
     var runner = Runner.create();
-
-    // run the engine
+    Render.run(render);
     Runner.run(runner, engine);
 
     // add mouse control
@@ -48,18 +69,39 @@ Hero.banner = function(){
         }
     });
 
-    
     Composite.add(world, mouseConstraint);
 
-    // keep the mouse in sync with rendering
-    render.mouse = mouse;
-}
+    render.mouse = mouse; // keep the mouse in sync with rendering
 
-Hero.banner.title = "Banner";
-Hero.banner.for = '>=0.14.2';
+    
+
+    //Resize Canvas so that the elements are crispy clear
+    function initializeCanvas(width, height) {
+
+        var canvas = document.getElementById('canvas');
+
+        canvas.width = width;
+        canvas.height = height;
+    
+        render.options.width  = width;
+        render.options.height = height;
+
+        render.canvas.width   = render.options.width;
+        render.canvas.height  = render.options.height;
+
+        Render.lookAt(render, {
+
+            min: { x: 0, y: 0 },
+            max: { x: width, y: height }
+
+        });
+
+        Render.setPixelRatio(render, "auto");
+    }
+
+    //Unhelpful for testing purposes, might have to make it more 'responsive' to see how it'd look on smaller screens
+    initializeCanvas(VIEW.width, (VIEW.height*0.75)) //Initializes the canvas to fit as a banner
+
+}
 
 Hero.banner()
-
-if (typeof module !== 'undefined') {
-    module.exports = Hero.banner;
-}
